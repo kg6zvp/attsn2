2ea88bad2679e955ebd5e1977e9859d0c327f65b
Sam McCollum <smccollum@mccollum.enterprises>
2017-11-21 19:11:54 -0800

    sm - configure more realistic API

--- src/main/java/jax/ScanJax.java
+++ src/main/java/jax/ScanJax.java
@@ -1,9 +1,12 @@
 package jax;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,8 +20,14 @@ public class ScanJax {
 	EntityManager em;
 
 	@GET
-	public Scan data() {
-		return em.find(Scan.class, 1);
+	@SuppressWarnings("unchecked")
+	public List<Scan> data() {
+		return (List<Scan>)em.createQuery("SELECT data FROM scans data").getResultList();
 	}
-
+	
+	@GET
+	@Path("/{id}")
+	public Scan get(@PathParam("id")Integer id) {
+		return em.find(Scan.class, id);
+	}
 }
